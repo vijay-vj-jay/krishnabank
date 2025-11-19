2836c6f51a3050b6dfedc4dc827b7a7c1a4859a9
krishnaDev <kmcrazybng@gmailc.com>
2025-11-19 08:13:51 +0530

    1st commit uploading application

---
+++ src/main/java/in/krishna/bank/WelcomeServlet.java
@@ -0,0 +1,41 @@
+package in.krishna.bank;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Krishna Bank - Welcome</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Welcome to Krishna Bank</h1>");
+        out.println("<p>Simple sample application running on Tomcat (Java 1.8).</p>");
+        out.println("<ul>");
+        out.println("<li>Customer Name: Krishna</li>");
+        out.println("<li>Account Number: 1234567890</li>");
+        out.println("<li>Current Balance: ₹ 50,000.00</li>");
+        out.println("</ul>");
+        out.println("<p><a href=\"index.jsp\">Back to Home</a></p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Krishna Bank - Home</title>
+</head>
+<body>
+<h1>Krishna Bank</h1>
+<p>This is a sample Java 8 web application deployed on Tomcat using Maven.</p>
+
+<ul>
+    <li><a href="welcome">View Sample Account Details</a></li>
+</ul>
+
+</body>
+</html>
